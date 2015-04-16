40a26a30f9f0e54bf555e110c91f98e2affca2d9
Joshua Angeley <willfire19@gmail.com>
2015-04-16 03:08:21 -0400

    Integrated Example Mars photosphere with Ruby on Rails

---
+++ config/initializers/assets.rb
@@ -0,0 +1,4 @@
+Rails.application.config.assets.precompile += %w( 360viewer.js )
+Rails.application.config.assets.precompile += %w( DeviceOrientationControls.js )
+Rails.application.config.assets.precompile += %w( OrbitControls.js )
+Rails.application.config.assets.precompile += %w( StereoEffect.js )

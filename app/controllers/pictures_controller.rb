ed0964402578f56c7d5674bd2c18814117b4af44
Joshua Angeley <willfire19@gmail.com>
2015-04-16 23:04:47 -0400

    Added user id to the picture model

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,7 @@
 class PicturesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
   before_action :set_picture, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   respond_to :html
 
@@ -22,8 +23,13 @@ class PicturesController < ApplicationController
   end
 
   def create
-    @picture = Picture.new(picture_params)
-    @picture.save
+    @picture = current_user.pictures.build(picture_params)
+    if @picture.save
+      flash[:success] = "Picture Created!"
+    end
+
+    # @picture = Picture.new(picture_params)
+    # @picture.save
     respond_with(@picture)
   end
 
@@ -38,6 +44,13 @@ class PicturesController < ApplicationController
   end
 
   private
+
+    def correct_user
+      @picture = current_user.pictures.find_by_id(params[:id])
+      redirect_to root_url if @picture.nil?
+      flash[:alert] = "That's not your picture!"
+    end
+
     def set_picture
       @picture = Picture.find(params[:id])
     end

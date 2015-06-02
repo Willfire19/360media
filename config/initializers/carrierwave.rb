e1ee472469ed795ff4965b0b1255f02889e98d6d
Joshua Angeley <willfire19@gmail.com>
2015-06-02 16:21:27 -0400

    Switched s3 bucket in carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -32,7 +32,7 @@ CarrierWave.configure do |config|
     # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
   # config.fog_directory  = ENV['S3_360MEDIA_BUCKET_NAME']                          # required
-  config.fog_directory  = 'media360-test'                          # required
+  config.fog_directory  = 'media360-pics'                          # required
   config.fog_public     = false                                        # optional, defaults to true
   # config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
   config.fog_attributes = {} # optional, defaults to {}

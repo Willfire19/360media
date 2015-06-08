0785698902b133ba3e20eac4a082deeb0287c0e1
willfire19 <willfire19@gmail.com>
2015-06-08 15:31:19 -0400

    Changed file upload destination for testing in carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,25 +16,25 @@ CarrierWave.configure do |config|
   # }
  
   # For testing, upload files to local `tmp` folder.
-  # if Rails.env.test? || Rails.env.development?
-  #   config.storage = :file
-  #   config.enable_processing = false
-  #   # config.root = "#{Rails.root}/tmp"
-  # else
-  config.storage = :fog
-  # config.fog_provider = 'fog' #'fog/aws'                        # required
-  config.fog_credentials = {
-    :provider              => 'AWS',                     # required
-    :aws_access_key_id     => ENV['S3_360MEDIA_KEY'],             # required
-    :aws_secret_access_key => ENV['S3_360MEDIA_SECRET'],
-    region:                'us-west-2',                  # optional, defaults to 'us-east-1'
-    # host:                  's3.example.com',             # optional, defaults to nil
-    # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
-  }
-  # config.fog_directory  = ENV['S3_360MEDIA_BUCKET_NAME']                          # required
-  config.fog_directory  = 'media360-pics'                          # required
-  config.fog_public     = false                                        # optional, defaults to true
-  # config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
-  config.fog_attributes = {} # optional, defaults to {}
-  # end
+  if Rails.env.test? || Rails.env.development?
+    config.storage = :file
+    config.enable_processing = false
+    # config.root = "#{Rails.root}/tmp"
+  else
+    config.storage = :fog
+    # config.fog_provider = 'fog' #'fog/aws'                        # required
+    config.fog_credentials = {
+      :provider              => 'AWS',                     # required
+      :aws_access_key_id     => ENV['S3_360MEDIA_KEY'],             # required
+      :aws_secret_access_key => ENV['S3_360MEDIA_SECRET'],
+      region:                'us-west-2',                  # optional, defaults to 'us-east-1'
+      # host:                  's3.example.com',             # optional, defaults to nil
+      # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+    }
+    # config.fog_directory  = ENV['S3_360MEDIA_BUCKET_NAME']                          # required
+    config.fog_directory  = 'media360-pics'                          # required
+    config.fog_public     = false                                        # optional, defaults to true
+    # config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+    config.fog_attributes = {} # optional, defaults to {}
+  end
 end
